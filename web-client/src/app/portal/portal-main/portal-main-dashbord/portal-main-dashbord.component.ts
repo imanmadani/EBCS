043b4a6bf2870beead31a56f4e03d2daf1a77e34
imanmadani <imn.madani@gmail.com>
2020-11-16 22:24:17 +0330

    iman

--- web-client/src/app/portal/portal-main/portal-main-dashbord/portal-main-dashbord.component.ts
+++ web-client/src/app/portal/portal-main/portal-main-dashbord/portal-main-dashbord.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {PortalMainDashboardService} from "./portal-main-dashboard.service";
 import * as moment from "jalali-moment";
 
@@ -10,23 +10,29 @@ import * as moment from "jalali-moment";
 export class PortalMainDashbordComponent implements OnInit {
   currency;
   exhibition;
-  constructor(private portalMainDashboardService:PortalMainDashboardService) { }
+
+  constructor(private portalMainDashboardService: PortalMainDashboardService) {
+  }
 
   ngOnInit(): void {
-    this.portalMainDashboardService.getLatestCurrency().subscribe(res=>{
+    this.portalMainDashboardService.getLatestCurrency().subscribe(res => {
       let nfObject = new Intl.NumberFormat('en-US');
-      let output = nfObject.format(res.data.row.UnitPrice);
-      this.currency=output;
-      this.portalMainDashboardService.getLatestExhibitions().subscribe(resEx=>{
-        debugger
-        this.exhibition=resEx.data.rows;
-        for(let i=0;i<2;i++){
-          if (this.exhibition[i].StartDateTime !== "0000-00-00 00:00:00") {
-            this.exhibition[i].StartDateTime = moment(this.exhibition[i].StartDateTime, 'YYYY/MM/DD hh:mm:ss').locale('fa').format('YYYY/MM/DD');
-            this.exhibition[i].EndDateTime = moment(this.exhibition[i].EndDateTime, 'YYYY/MM/DD hh:mm:ss').locale('fa').format('YYYY/MM/DD');
-          }else {
-            this.exhibition[i].StartDateTime ="-";
-            this.exhibition[i].EndDateTime ="-";
+      let output = 'نامشخص';
+      if (res.data.row) {
+        output = nfObject.format(res.data.row.UnitPrice);
+      }
+      this.currency = output;
+      this.portalMainDashboardService.getLatestExhibitions().subscribe(resEx => {
+        this.exhibition = resEx.data.rows;
+        if (this.exhibition) {
+          for (let i = 0; i < 2; i++) {
+            if (this.exhibition[i].StartDateTime !== "0000-00-00 00:00:00") {
+              this.exhibition[i].StartDateTime = moment(this.exhibition[i].StartDateTime, 'YYYY/MM/DD hh:mm:ss').locale('fa').format('YYYY/MM/DD');
+              this.exhibition[i].EndDateTime = moment(this.exhibition[i].EndDateTime, 'YYYY/MM/DD hh:mm:ss').locale('fa').format('YYYY/MM/DD');
+            } else {
+              this.exhibition[i].StartDateTime = "-";
+              this.exhibition[i].EndDateTime = "-";
+            }
           }
         }
       });
